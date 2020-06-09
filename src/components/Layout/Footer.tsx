23f745551ba70207c60b2e861c68bd1218c0340e
Eliseu Videira <eliseuvideira1@gmail.com>
2020-06-08 22:36:32 -0300

    Some fixing in footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,39 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Paragraph = styled.p`
-  a,
-  a:visited,
-  a:hover,
-  a:active {
-    color: inherit;
-    text-decoration: none;
-  }
-`;
-
 const StyledFooter = styled.footer`
+  display: flex;
   position: relative;
   height: 80px;
-  margin-top: -80px;
-  padding-top: 40px;
-  padding-bottom: 10px;
-  div {
-    display: flex;
-    align-items: center;
-    height: 100%;
+  margin: -80px 0 0;
+  padding: 40px 0 10px;
+  align-items: center;
+  p {
+    margin: 0;
+  }
+
+  a:hover,
+  a:focus {
+    text-decoration: underline;
   }
 `;
 
 const Footer: React.FC = () => (
   <StyledFooter style={{ height: '80px' }}>
-    <div className="text-muted">
-      <Paragraph>
-        &copy; {new Date().getFullYear()}, Built with{' '}
-        <a href="https://www.gatsbyjs.org" target="_blank" rel="noopener">
-          Gatsby
-        </a>
-      </Paragraph>
-    </div>
+    <p className="text-muted">
+      &copy; {new Date().getFullYear()}, Built with{' '}
+      <a href="https://www.gatsbyjs.org" target="_blank" rel="noopener">
+        Gatsby
+      </a>
+    </p>
   </StyledFooter>
 );
 
